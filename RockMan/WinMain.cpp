8b3081dbb9f08d1ffb10486f33ee060f5356ea58
vuhoang1701 <Vuhoang1993>
2015-08-28 21:20:14 +0700

    sYNCE COMPARE
    ABC

--- RockMan/WinMain.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <windows.h>
-#include "CGameApp.h"
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-	CGameApp* game = new CGameApp(hInstance);
-	game->Run();
-	return 0;
-}
